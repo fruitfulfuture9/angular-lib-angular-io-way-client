24ac057d7544fcde6aaa41bc5dabe195470dbdcf
fruitfulfuture9 <fruitfulfuture9@gmail.com>
2021-06-28 20:12:03 -0700

    working copy after importing jagan-lib generated using angular io way of library creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { JaganLibModule} from 'jagan-lib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    JaganLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
